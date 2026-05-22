b0a0c52aad41a3e09b9613c1d43cde87fa03ec9c
Bao Khang App Mobile <206207413+mrtinkcf@users.noreply.github.com>
2026-05-22 09:11:52 +0700

    Add iOS entry point for Revenue app

---
+++ BKPos.Revenue.App/Platforms/iOS/Program.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace BKPos.Revenue.App;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
